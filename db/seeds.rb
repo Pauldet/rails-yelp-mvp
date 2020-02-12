6a31c554e74c1865911aa520b57773f359fa289d
Paul de Thore <p_dethore@hotmail.com>
2020-02-12 15:44:26 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Restaurant.create(name: "Bristol", category: "french", address: "rue du Faubourg Saint Honoré", phone_number: "01 53 43 43 00")
+Restaurant.create(name: "Mamma Mia", category: "italian", address: "rue Oberkampf", phone_number: "01 98 34 27 00")
+Restaurant.create(name: "Le lotus bleu", category: "chinese", address: "avenue de Choisy", phone_number: "01 40 20 78 83")
+Restaurant.create(name: "Kyushu délices", category: "japanese", address: "rue Sainte Anne", phone_number: "03 49 48 29 30")
+Restaurant.create(name: "La baraque à frite", category: "belgian", address: "rue Saint Denis", phone_number: "01 29 10 36 12")
+
